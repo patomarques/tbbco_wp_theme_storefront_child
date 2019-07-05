6dba4d77abf69745caf870dd23475c53144f5b01
Pato Marques <patoxmarques@gmail.com>
2019-07-05 14:45:07 -0300

    slider custom home

--- wp-content/themes/storefront-child/js/tbbc.js
+++ wp-content/themes/storefront-child/js/tbbc.js
@@ -10,6 +10,18 @@
       }, 3000);
     });*/
 
+    $("#owl-demo").owlCarousel({
+      navigation : true,
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem: true,
+      pagination: false,
+      rewindSpeed: 500,
+      items : 1,
+      loop:true,
+      nav:true,
+      autoHeight:true
+    });
   });
 
   $(function () {
